12c585367ea0ef99178344213e4073ad5fdfadbf
Priyanshu Panwar <priyanshugurjar066@gmail.com>
2024-11-23 20:11:59 +0530

    Add files via upload

---
+++ Weather/Weather-App.js
@@ -0,0 +1,43 @@
+const ApiKey = "825e839bd3aed374664853df12f8a90b";
+const ApiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function CheckWeather(city) {
+    const response = await fetch(ApiUrl + city + `&appid=${ApiKey}`);
+
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        var data = await response.json();
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML =
+            Math.round(data.main.temp) + "°C";
+        document.querySelector(".Humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".Wind").innerHTML = data.wind.speed + "km/h";
+
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "cloudy (1).png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "sun.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "heavy-rain.png";
+        } else if (data.weather[0].main == "Snow") {
+            weatherIcon.src = "snowy_2044021.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "cloudy.png";
+        }
+        document.querySelector(".weather").style.display = "Block";
+        document.querySelector(".error").style.display = "none";
+    }
+}
+
+searchbtn.addEventListener("click", () => {
+    CheckWeather(searchBox.value);
+});
+
+CheckWeather();
