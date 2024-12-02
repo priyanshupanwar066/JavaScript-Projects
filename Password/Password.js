f1dae61e7daf05eca78d7ccb9af96fc9d605f4c6
Priyanshu Panwar <priyanshugurjar066@gmail.com>
2024-12-02 06:39:26 +0530

    Add files via upload

---
+++ Password/Password.js
@@ -0,0 +1,14 @@
+let eyeicon = document.getElementById('eyeicon');
+let password = document.getElementById('password');
+
+
+eyeicon.onclick = function(){
+    if(password.type === 'password'){
+        password.type = 'text';
+        eyeicon.src = "eye.png";
+    } else {
+        password.type = 'password'; 
+        eyeicon.src = "hidden.png";
+    }
+ 
+}
